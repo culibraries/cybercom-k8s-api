88ead5c4049e52130f20a7c1ccc3ad2408f5e736
Mark Stacy <mbstacy@gmail.com>
2019-06-17 09:38:08 -0600

    udated urls and db connections

--- api/urls.py
+++ api/urls.py
@@ -25,18 +25,18 @@ admin.site.site_title = _(config.APPLICATION_TITLE)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     # Root API
-    path("",APIRoot.as_view()),
+    path("api/",APIRoot.as_view()),
     #Authentication and Admin
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     re_path(r'^api/token/$', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     re_path(r'^api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
     re_path(r'^api/token/verify/$', TokenVerifyView.as_view(), name='token_verify'),
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     # Cybercommons Django Apps
-    path('queue/', include('cybercom_queue.urls')),
-    path('data_store/',include('data_store.urls')),
-    path('catalog/',include('catalog.urls')),
-    path('user/',UserProfile.as_view(),name='user-list'),
+    path('api/queue/', include('cybercom_queue.urls')),
+    path('api/data_store/',include('data_store.urls')),
+    path('api/catalog/',include('catalog.urls')),
+    path('api/user/',UserProfile.as_view(),name='user-list'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
