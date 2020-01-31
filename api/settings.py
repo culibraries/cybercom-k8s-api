9b718a6823f3904e0aca65fc492a5c0be9722570
Mark Stacy <mbstacy@gmail.com>
2020-01-31 15:05:09 -0700

    learn how to spell

--- api/settings.py
+++ api/settings.py
@@ -288,7 +288,7 @@ SAML_PROVIDERS = [{
         "debug": False,
         "custom_base_path": "",
         "sp": {
-            "entityId": "{0}/api/api-saml/sso/saml/metadata".foramt(os.getenv('SAML_HOST_BASE_URL')),
+            "entityId": "{0}/api/api-saml/sso/saml/metadata".format(os.getenv('SAML_HOST_BASE_URL')),
             "assertionConsumerService": {
                 "url": "{0}/api/api-saml/sso/saml/?acs".format(os.getenv('SAML_HOST_BASE_URL')),
                 "binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
